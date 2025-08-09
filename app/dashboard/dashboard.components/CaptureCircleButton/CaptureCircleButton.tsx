865e3682aa2486e1932ac0f12d5367f1068c16ec
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-09 04:05:10 +0300

    Add circle button

---
+++ app/dashboard/dashboard.components/CaptureCircleButton/CaptureCircleButton.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import styled from '@emotion/styled';
+
+export const CaptureCircleButton = styled.button`
+  width: 220px;
+  height: 220px;
+  border-radius: 50%;
+  background-color: orange;
+  color: white;
+  border: none;
+  cursor: pointer;
+  font-size: 1.2rem;
+`;
