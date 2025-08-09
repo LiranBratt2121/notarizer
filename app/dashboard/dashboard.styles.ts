f5b0835934a2a6cd76b09a4babbd5e549ab7705a
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-09 04:29:37 +0300

    Add dashboard container

---
+++ app/dashboard/dashboard.styles.ts
@@ -0,0 +1,13 @@
+"use client";
+
+import styled from '@emotion/styled';
+
+export const DashboardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 6rem;
+  height: 100vh;
+  width: 100%vw;
+`
