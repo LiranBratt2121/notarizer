5e5db64d6ed06831331848d1c25ae25e46d06fd7
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-09 18:50:32 +0300

    Add container for add styles

---
+++ app/apartments/add/add.styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const AddApartmentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  gap: 20px;
+`;
