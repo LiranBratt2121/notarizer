408a4b3ba79695de7ae832e45f0b0c6f3c81544e
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-09 18:49:39 +0300

    Display username from firebase auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,22 @@
+"use client";
+
 import React from 'react'
 import { CaptureCircleButton } from './dashboard.components/CaptureCircleButton/CaptureCircleButton';
 import { DashboardContainer } from './dashboard.styles';
 import HeaderAbs from '../../components/Text/HeaderAbs/HeaderAbs';
+import { useAuth } from '@/hooks/useAuth';
 
 const Dashboard = () => {
+  const { user } = useAuth();
+  
+  if (!user) {
+    return <div>Please log in to access the dashboard.</div>;
+  }
+
+  console.log('User:', user);
   return (
     <DashboardContainer>
-      <HeaderAbs text='Hello, User'/>
+      <HeaderAbs text={`Hello, ${user.displayName}`}/>
       <CaptureCircleButton>
         Capture
       </CaptureCircleButton>
