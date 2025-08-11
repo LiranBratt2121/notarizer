655bfabc8450d5e79741b183b03ba1a4985ebb89
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-11 12:30:04 +0300

    Center the header

--- components/Text/Header/Header.styles.ts
+++ components/Text/Header/Header.styles.ts
@@ -4,5 +4,6 @@ import styled from "@emotion/styled";
 
 export const HeaderStyled = styled.h1<{headercolor?: string, size?: string}>`
   font-size: ${props => props.size || '2rem'};
+  text-align: center;
   color: ${props => props.headercolor || 'black'};
 `
