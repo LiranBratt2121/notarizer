e001ebfc7d62105a3a431acb6943f5a13493a6d8
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-13 05:22:51 +0300

    Redirect from / to dashboard

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        pathname: "/uc**"
+      }
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
