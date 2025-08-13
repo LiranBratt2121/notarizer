02ab135e9ebff2dc03b9b77bf844b473f5f7d3c5
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-13 05:28:16 +0300

    Use address card list

--- app/apartments/page.tsx
+++ app/apartments/page.tsx
@@ -4,14 +4,29 @@ import React from 'react'
 import { ApartmentContainer } from './apartments.styles';
 import Button from '@/components/Buttons/Button/Button';
 import { useRouter } from 'next/navigation';
+import AddressCardList from './apartments.components/AddressCardList/AddressCardList';
+import Header from '@/components/Text/Header/Header';
+import { useAuth } from '@/hooks/useAuth';
+import LoginNotice from '@/components/LoginNotice/LoginNotice';
+import { FirebaseApartment } from '@/types/apartment';
+import { useUserApartments } from '@/hooks/useUserApartments';
+
+
+// const apartments: FirebaseApartment[] = [
+//   { id: '1', street: '123 Main St', city: 'Springfield', state: 'IL', postalCode: '62701', country: 'USA', images: [] },
+//   { id: '2', street: '456 Elm St', city: 'Shelbyville', state: 'IL', postalCode: '62565', country: 'USA', images: [] },
+//   { id: '3', street: '789 Oak St', city: 'Capital City', state: 'IL', postalCode: '62702', country: 'USA', images: [] },
+// ]
 
 const Apartments = () => {
   const navigate = useRouter();
+  const { user } = useAuth();
+  const { apartments } = useUserApartments();
   
   const handleAddApartment = () => {
     navigate.replace('/apartments/add');
   }
-  
+
   if (!user) {
     return (
       <LoginNotice
@@ -20,11 +35,15 @@ const Apartments = () => {
       />
     )
   }
+
   return (
     <ApartmentContainer>
-        <Button width='80%' height='5rem' onClick={handleAddApartment}>
-            Add Apartment
-        </Button>
+      <Header text={apartments.length > 0 ? `${user.displayName}'s Apartments` : "No apartments available"} />
+      <AddressCardList apartments={apartments} />
+
+      <Button width='80%' height='5rem' onClick={handleAddApartment}>
+        Add Apartment
+      </Button>
     </ApartmentContainer>
   )
 }
