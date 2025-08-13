e0bbe47e7da5c507352b564cd87816a5ae566d52
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-13 05:23:01 +0300

    Add loading normal file

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import LoadingCircle from '@/components/LoadingBar/LoadingCircle/LoadingCircle'
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <LoadingCircle/>
+  )
+}
+
+export default Loading
