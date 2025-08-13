52bd9969e9e89304497feaffd06c5651b7f028a8
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-13 05:27:00 +0300

    Improve upload image logic

--- hooks/useGoogleDrive.ts
+++ hooks/useGoogleDrive.ts
@@ -2,10 +2,22 @@
 
 export function useGoogleDrive() {
     async function uploadImage(base64Image: string, fileName: string) {
+        const byteString = atob(base64Image);
+        const arrayBuffer = new ArrayBuffer(byteString.length);
+        const intArray = new Uint8Array(arrayBuffer);
+
+        for (let i = 0; i < byteString.length; i++) {
+            intArray[i] = byteString.charCodeAt(i);
+        }
+
+        const blob = new Blob([intArray], { type: "image/png" });
+
+        const formData = new FormData();
+        formData.append("file", blob, fileName);
+
         const response = await fetch("/api/uploadImage", {
             method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ base64Image, fileName }),
+            body: formData
         });
 
         if (!response.ok) {
@@ -43,14 +55,26 @@ export function useGoogleDrive() {
         return response.json();
     }
 
+    async function fetchDriveImageBase64(fileId: string) {
+        const res = await fetch(`/api/driveToBase64?fileId=${encodeURIComponent(fileId)}`);
+        if (!res.ok) {
+            throw new Error("Failed to get Base64 from server");
+        }
+
+        const data = await res.json();
+
+        return data.base64 as string;
+    }
+
     async function uploadAndGetUrl(base64Image: string, imageName: string) {
         const { id } = await uploadImage(base64Image, imageName);
         const { webViewLink, webContentLink } = await generatePublicImageUrl(id);
 
         return {
-            webViewLink, webContentLink
+            webViewLink,
+            webContentLink
         }
     }
 
-    return { uploadImage, deleteImage, generatePublicImageUrl, uploadAndGetUrl };
+    return { uploadImage, deleteImage, generatePublicImageUrl, uploadAndGetUrl, fetchDriveImageBase64};
 }
