b1c8a2bdc2aa40d6459362ac9d44bf09a5bd8008
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-13 05:26:35 +0300

    Add test to unreachable / page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { useGoogleDrive } from "@/hooks/useGoogleDrive";
 
 const Page = () => {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  )
-}
+  const { uploadAndGetUrl, uploadImage } = useGoogleDrive();
 
-export default Page
+  React.useEffect(() => {
+    uploadImage("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=", "fileName").then((response) => {
+      console.log(response);
+    });
+  }, []);
+
+  return <h1>Hello world</h1>;
+};
+
+export default Page;
