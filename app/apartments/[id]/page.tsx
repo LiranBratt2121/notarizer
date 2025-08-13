4e5b17db91b963e5ba1267bce98167ce00c1d757
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-13 05:28:39 +0300

    Add basic Image viewer

---
+++ app/apartments/[id]/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import React, { useCallback, useMemo } from 'react'
+import { useParams } from 'next/navigation';
+import { useAuth } from '@/hooks/useAuth';
+import LoginNotice from '@/components/LoginNotice/LoginNotice';
+import { useUserApartments } from '@/hooks/useUserApartments';
+import Notice from '@/components/Notice/Notice';
+import { AddApartmentContainer } from '../add/add.styles';
+import ImageCardList from '@/components/ImageCard/ImageCardList/ImageCardList';
+import Header from '@/components/Text/Header/Header';
+
+const ImageGallery = () => {
+    const { id } = useParams();
+    const { user } = useAuth();
+    const { apartments } = useUserApartments();
+
+    const isUsersApartment = useCallback(
+        () => apartments.some((apartment) => apartment.id === id),
+        [apartments, id]
+    );
+
+    const currentApartment = useMemo(() => apartments.find(apartment => apartment.id === id), [id, apartments]);
+
+    if (!user) {
+        return (
+            <LoginNotice
+                headerMessage="Please log in to access your capture dashboard."
+                buttonMessage="Log In"
+            />
+        )
+    }
+
+    if (!isUsersApartment()) {
+        return (
+            <Notice
+                headerMessage="You do not have access to this apartment."
+                buttonMessage="Go Back"
+                href={'/apartments/'}
+            />
+        )
+    }
+
+    if (!currentApartment) {
+        return (
+            <Notice
+                headerMessage="Apartment not found."
+                buttonMessage="Go Back"
+                href={'apartments/'}
+            />
+        )
+    }
+
+    return (
+        <AddApartmentContainer>
+            <Header text={`${currentApartment.street} Images`} />
+            <ImageCardList images={currentApartment.images} />
+        </AddApartmentContainer>
+    )
+}
+
+export default ImageGallery;
