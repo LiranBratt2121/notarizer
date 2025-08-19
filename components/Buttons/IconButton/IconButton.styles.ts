ea570009cd724f8a904b0f8e53fce2fb47532f59
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-20 01:11:06 +0300

    Remove abs pos

--- components/Buttons/IconButton/IconButton.styles.ts
+++ components/Buttons/IconButton/IconButton.styles.ts
@@ -1,8 +1,8 @@
 import styled from "@emotion/styled";
 
 export const Icon = styled.button`
-  position: absolute;
   background: transparent;
+  
   border: none;
   cursor: pointer;
   padding: 0;
