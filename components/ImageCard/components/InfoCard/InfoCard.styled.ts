43adb0ed990096d1de6195cc3b65453ebd0bc159
LiranBratt2121 <brattlirannin@gmail.com>
2025-08-22 18:16:06 +0300

    Add info card under images

---
+++ components/ImageCard/components/InfoCard/InfoCard.styled.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled"
+
+export const InfoWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 8px;
+    padding-top: 2px;
+    background-color: rgba(255, 141, 11, 1);
+
+    border-radius: 0 0 10px 10px;
+    border: 1px solid black;
+`
